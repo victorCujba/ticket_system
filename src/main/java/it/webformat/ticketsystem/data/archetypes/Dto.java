2e51661007ed76ccd54b7fdde4805f0e4b9271b1
cujbaVictor <cujba.victor96@gmail.com>
2023-12-27 17:20:58 +0100

    archetypes generated.

---
+++ src/main/java/it/webformat/ticketsystem/data/archetypes/Dto.java
@@ -0,0 +1,7 @@
+package it.webformat.ticketsystem.data.archetypes;
+
+
+public interface Dto {
+    Model toModel();
+
+}
---
+++ src/main/java/it/webformat/ticketsystem/data/archetypes/Model.java
@@ -0,0 +1,5 @@
+package it.webformat.ticketsystem.data.archetypes;
+
+public interface Model {
+    Dto toDto();
+}
