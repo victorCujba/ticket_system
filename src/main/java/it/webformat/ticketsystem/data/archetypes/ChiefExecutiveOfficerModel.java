19eb801fd437ceb5bc2e8917867b860e75fa1346
cujbaVictor <cujba.victor96@gmail.com>
2023-12-28 18:54:45 +0100

    new archetypes added.

---
+++ src/main/java/it/webformat/ticketsystem/data/archetypes/ChiefExecutiveOfficerModel.java
@@ -0,0 +1,7 @@
+package it.webformat.ticketsystem.data.archetypes;
+
+public interface ChiefExecutiveOfficerModel {
+
+    Dto toCeoDto();
+
+}
---
+++ src/main/java/it/webformat/ticketsystem/data/archetypes/ProjectManagerModel.java
@@ -0,0 +1,7 @@
+package it.webformat.ticketsystem.data.archetypes;
+
+public interface ProjectManagerModel {
+
+    Dto toPmDto();
+
+}
