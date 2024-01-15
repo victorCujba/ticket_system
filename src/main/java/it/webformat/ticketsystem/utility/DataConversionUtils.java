d958ba33720865c33c5ab796b1185bde13ef331e
cujbaVictor <cujba.victor96@gmail.com>
2024-01-15 09:21:59 +0100

    Small refactoring. 7

--- src/main/java/it/webformat/ticketsystem/utility/DataConversionUtils.java
+++ src/main/java/it/webformat/ticketsystem/utility/DataConversionUtils.java
@@ -27,38 +27,9 @@ public class DataConversionUtils {
         return value == null ? null : value.toString();
     }
 
-    public static Boolean stringToBoolean(String value) {
-        return value == null ? null : Boolean.valueOf(value);
-    }
-
-    public static String booleanToString(Boolean value) {
-        return value == null ? null : value.toString();
-    }
-
-    public static EmployeeRole stringToEmployeeRole(String value) {
-        for (EmployeeRole priorityValue : EmployeeRole.values()) {
-            if (priorityValue.name().equalsIgnoreCase(value))
-                return priorityValue;
-        }
-        return null;
-    }
-
     public static String employeeRoleToString(EmployeeRole value) {
         return value == null ? null : value.name();
     }
-
-
-//    public static ListLabel stringToListLabel(String value) {
-//        for (ListLabel cardStatusValue : ListLabel.values()) {
-//            if (cardStatusValue.name().equalsIgnoreCase(value))
-//                return cardStatusValue;
-//        }
-//        return null;
-//    }
-//
-//    public static String listLabelToString(ListLabel value) {
-//        return value == null ? null : value.name();
-//    }
 
 
 }
